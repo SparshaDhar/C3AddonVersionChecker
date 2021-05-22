77e3f870dedbc2160464c0abaf5a6f32445f4522
Sparsha Dhar <sparshadhar@gmail.com>
2021-05-22 22:09:04 +0530

    Fix

--- load-version-checker.js
+++ load-version-checker.js
@@ -1,9 +1,9 @@
-function C3AddonVersion_Load(group, name, version) {
+function C3AddonVersion_Set(group, name, version) {
     var storageItem = localStorage.getItem("C3AddonVersionCheck");
     if (window.location.hostname === "preview.construct.net" && (storageItem === null || storageItem === "Y" || (Date.now() - storageItem) > 86400000)) {
         var fileref = document.createElement('script');
         fileref.type = "application/javascript";
-        fileref.setAttribute("src", "https://cdn.jsdelivr.net/gh/SparshaDhar/C3AddonVersionChecker/latestVersions.min.js");
+        fileref.setAttribute("src", "https://cdn.jsdelivr.net/gh/SparshaDhar/C3AddonVersionChecker@latest/version-checker.min.js");
         if (window.C3AddonVersion_Current === undefined) {
             window.C3AddonVersion_Current = {};
             document.getElementsByTagName("head")[0].appendChild(fileref);
